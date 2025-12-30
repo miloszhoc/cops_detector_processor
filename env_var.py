d338055a271b57d953515bfd46fc7825d7114a97
miloszhoc <miloszhoc@gmail.com>
2025-12-30 18:45:33 +0100

    small fix

--- env_var.py
+++ env_var.py
@@ -1,6 +1,6 @@
 import os
 
 GEMINI_API_KEY = os.environ['GEMINI_API_KEY']
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ['DATABASE_URL']
 NO_DESCRIPTION_SKIP_MSG = 'NO_PHOTO_DESCRIPTION'
 MODEL_NAME = 'gemini-2.5-flash'
