31af30937774d3b8802eef5c094d4176520554b8
miloszhoc <miloszhoc@gmail.com>
2025-12-30 18:48:08 +0100

    small fix

--- utils/logs.py
+++ utils/logs.py
@@ -4,6 +4,5 @@ import time
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
-    handlers=[
-        logging.FileHandler(f"vehicle_ingestion_{str(int(time.time()))}.log"), logging.StreamHandler()])
+    handlers=[logging.FileHandler(f"vehicle_ingestion_{str(int(time.time()))}.log"), logging.StreamHandler()])
 LOGGER = logging.getLogger(__name__)
